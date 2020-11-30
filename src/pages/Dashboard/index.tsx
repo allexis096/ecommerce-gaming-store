11ee03ad6d7e2e7447f6c372193f698ffac81ac8
allexis096 <allexissoares@gmail.com>
2020-11-30 20:55:41 -0300

    :zap: chore: Dashboard static page stylized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { Text } from 'react-native';
-
-import { Container } from './styles';
+import {
+  Products,
+  Card,
+  CardImage,
+  CardName,
+  CardDivide,
+  CardPrice,
+  CardScore,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Container>
-      <Text>Dashboard</Text>
-    </Container>
+    <SafeAreaView>
+      <Products>
+        <Card>
+          <CardImage source={require('../../assets/super-mario-odyssey.png')} />
+          <CardName>Super Mario Odyssey</CardName>
+          <CardScore>Nota: 100</CardScore>
+          <CardDivide>
+            <CardPrice>R$197,88</CardPrice>
+            <TouchableOpacity>
+              <FeatherIcon
+                name="plus"
+                color="#fff"
+                size={30}
+                style={{ backgroundColor: '#08AF0A', borderRadius: 5 }}
+              />
+            </TouchableOpacity>
+          </CardDivide>
+        </Card>
+      </Products>
+    </SafeAreaView>
   );
 };
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Products = styled.View`
+  margin: 0 5px;
+`;
+
+export const Card = styled.View`
+  background-color: #464648;
+  max-width: 45%;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+export const CardImage = styled.Image`
+  width: 150px;
+  height: 150px;
+  margin-top: 5px;
+`;
+
+export const CardName = styled.Text`
+  font-family: 'Roboto-Regular';
+  color: #fff;
+  margin: 10px 0;
+`;
+
+export const CardScore = styled.Text`
+  font-family: 'Roboto-Regular';
+  color: #fff;
+  background-color: #0e0e10;
+  padding: 5px;
+  border-radius: 5px;
+`;
+
+export const CardDivide = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px 0;
+`;
+
+export const CardPrice = styled.Text`
+  font-family: 'Roboto-Regular';
+  color: #fff;
+  font-size: 20px;
+  margin-right: 20px;
+`;
