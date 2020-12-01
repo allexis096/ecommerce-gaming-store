08031c1f250f460785bf4e2e4d9843436f03889b
allexis096 <allexissoares@gmail.com>
2020-12-01 17:16:38 -0300

    :bulb: chore: using context of useStore hook on App file

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { GameProvider } from './hooks/store';
 import AppRoutes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="dark-content" backgroundColor="#18181B" />
-    <AppRoutes />
+    <GameProvider>
+      <StatusBar barStyle="dark-content" backgroundColor="#18181B" />
+      <AppRoutes />
+    </GameProvider>
   </NavigationContainer>
 );
 
