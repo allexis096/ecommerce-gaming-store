2faca078a98bec7903f4b3c09e5f7b3d8088ec25
allexis096 <allexissoares@gmail.com>
2020-12-01 17:14:09 -0300

    :sparkles: feat: util function formatValue

---
+++ src/utils/formatValue.ts
@@ -0,0 +1,9 @@
+import 'intl/locale-data/jsonp/pt-BR';
+
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+export default formatValue;
