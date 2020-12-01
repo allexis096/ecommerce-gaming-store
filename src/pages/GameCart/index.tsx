a3294ef9329c0b385c52078442148cb8e704be64
allexis096 <allexissoares@gmail.com>
2020-12-01 17:14:37 -0300

    :sparkles: feat: add GameCart page

---
+++ src/pages/GameCart/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const GameCart: React.FC = () => {
+  return (
+    <Container>
+      <Text>GameCard</Text>
+    </Container>
+  );
+};
+
+export default GameCart;
